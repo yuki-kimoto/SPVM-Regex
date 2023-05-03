ce50436dd94149899e03ef09052aa4b2ed4665a6
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-05-04 08:55:51 +0900

    The Regex::Match class is added

---
+++ lib/SPVM/Regex/Match.pm
@@ -0,0 +1,45 @@
+package SPVM::Regex::Match;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Regex::Match - Short Description
+
+=head1 Description
+
+The Regex::Match class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Regex::Match;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
