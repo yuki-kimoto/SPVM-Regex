7ba152487e42a99bae8bac4c8dec129faa379fbb
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-05-04 10:36:35 +0900

    The info option is added

---
+++ lib/SPVM/Regex/ReplaceInfo.pm
@@ -0,0 +1,45 @@
+package SPVM::Regex::ReplaceInfo;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Regex::ReplaceInfo - Short Description
+
+=head1 Description
+
+The Regex::ReplaceInfo class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Regex::ReplaceInfo;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
