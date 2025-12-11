6bc956281643cf92890081005192e8e9a087b5d9
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-12-11 10:32:13 +0900

    Refactor c code.

--- lib/SPVM/Regex.cpp
+++ lib/SPVM/Regex.cpp
@@ -89,7 +89,8 @@ int32_t SPVM__Regex__match_string(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t doller0_and_captures_length = captures_length + 1;
   
   std::vector<re2::StringPiece> submatch(doller0_and_captures_length);
-  int32_t match = re2->Match(string, offset, offset + length, re2::RE2::Anchor::UNANCHORED, submatch.data(), doller0_and_captures_length);
+  int32_t end_offset = offset + length;
+  int32_t match = re2->Match(string, offset, end_offset, re2::RE2::Anchor::UNANCHORED, submatch.data(), doller0_and_captures_length);
   
   void* obj_regex_match = NULL;
   if (match) {
